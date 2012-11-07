97533dc8de9f20c2dd314451a9040ef452e78b16
ekhatko <ekhatko@at-consulting.ru>
2012-11-07 16:03:02 +0400

    assets precompiled

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,7 @@ set :deploy_to, "/home/#{user}/#{application}"
 set :use_sudo, false
 
 default_run_options[:pty] = true
-default_environment['LD_LIBRARY_PATH'] = '/usr/lib/oracle/11.2/client64/lib/'
-default_environment['ORACLE_HOME']='/usr/lib/oracle/11.2/client64/'
+default_environment['LD_LIBRARY_PATH'] = "#{ENV["ORACLE_HOME"]}/lib/"
 default_environment['NLS_LANG']='AMERICAN_CIS.UTF8'
 
 set :repository, "git@github.com:evgeniy-khatko/tarantula.git"
@@ -19,23 +18,28 @@ set :branch, "master"
 set :scm, "git"
 
 set :scm_verbose, true
-set :deploy_via, :remote_cache
 
-set :bundle_flags, "--quiet"
+#set :bundle_flags, "--quiet"
 
 set :app_port, 80
 
 namespace :deploy do
- task(:start) {}
- task(:stop) {}
+	task(:start) {}
+	task(:stop) {}
 
- desc 'Restart Application'
- task :restart, :roles => :app, :except => { :no_release => true } do
+	desc 'Restart Application'
+	task :restart, :roles => :app, :except => { :no_release => true } do
 	 run "#{try_sudo} touch #{File.join current_path,'tmp','restart.txt'}"
- end
+	end
 
- task :my, :roles => :app do
+	desc "Symlinks the database.yml"
+	task :symlink_db, :roles => :app do
+		run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+	end
+
+	task :my, :roles => :app do
 	 run "echo 1"
- end
+	end
 end
 
+after 'deploy:update_code', 'deploy:symlink_db'
