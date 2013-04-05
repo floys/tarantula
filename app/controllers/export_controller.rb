01c4451ae04595e623453481555f9bcb0b8bde61
ekhatko <ekhatko@at-consulting.ru>
2013-04-05 21:37:54 +0400

    report updates

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -16,6 +16,7 @@ class ExportController < ApplicationController
       puts cases.collect(&:title)
       export_cases cases
       name = e.name
+    when 'Tag'
     end
     path = Rails.public_path + "/reports/"
     @report.generate(path)
@@ -26,19 +27,6 @@ class ExportController < ApplicationController
 
   def export_cases cases
     @report = ODFReport::Report.new("#{Rails.public_path}/export.odt") do |r|
-      #cases.each{ |c|
-      #  r.add_field :test_title, c.title
-      #  r.add_field :test_objective, c.objective
-      #  r.add_field :test_preconditions, c.preconditions_and_assumptions
-      #  r.add_field :test_data, c.test_data
-      #  index = 0
-      #  r.add_table("STEPS", c.steps, :header => true) do |t|
-      #    t.add_column(:step_index){ |item| StepExecution.find_by_step_id(item.id).position.to_s }
-      #    t.add_column :step_action, :action
-      #    t.add_column :step_result, :result
-      #  end
-      #}
-
       r.add_section("CASES", cases) do |s|
         s.add_field(:test_title){|item| item.title}
         s.add_field :test_objective, :objective
@@ -46,6 +34,7 @@ class ExportController < ApplicationController
         s.add_field :test_data, :test_data
 
         s.add_table("STEPS", :steps, :header => true) do |t|
+          t.add_column(:step_index){ |item| StepExecution.find_by_step_id(item.id).position.to_s }
           t.add_column :step_action, :action
           t.add_column :step_result, :result
         end
